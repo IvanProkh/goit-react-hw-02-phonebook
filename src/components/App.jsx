dfaf917148ea3c1497c0fb54ae2141018e5f0f5b
IvanProkhorets <prohorets.ivan@gmail.com>
2022-10-09 00:03:03 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,39 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {
     contacts: [],
+    filter: '',
     name: '',
+    number: '',
   };
 
-  handleNameChange = event => {
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
     this.setState({
-      name: event.currentTarget.value,
+      [name]: value,
     });
+  };
+
+  handleNumberChange = e => {
+    this.setState({
+      number: e.currentTarget.number,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // this.setState({
+    //   this.state.contact.push(e.currentTarget)
+
+    // });
   };
 
   render() {
     return (
       <div>
+        {/* console.log("~ state", state) */}
         <h1>Phonebook</h1>
-
-        <form className="App">
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name
             <input
@@ -26,11 +44,21 @@ class App extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               value={this.state.name}
-              onChange={this.handleNameChange}
+              onChange={this.handleChange}
               required
             />
           </label>
-          <button>Add contact</button>
+          <label>
+            Number
+            <input
+              type="number"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         <ul>
