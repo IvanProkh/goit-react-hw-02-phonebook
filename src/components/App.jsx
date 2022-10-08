b5ccb72a78f2f82672566c2a2cd878d679281862
IvanProkhorets <prohorets.ivan@gmail.com>
2022-10-08 21:53:12 +0300

    добавил nanoid, styled-components и начальную разметку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,44 @@
-const App = () => {
-  return <div></div>;
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({
+      name: event.currentTarget.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+
+        <form className="App">
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              required
+            />
+          </label>
+          <button>Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li>contact</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
